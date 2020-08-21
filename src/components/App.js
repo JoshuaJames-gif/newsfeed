ee3af5c592c4f50c2c26dc349f0a23d190187c8c
Kojjob <kodeon6@gmail.com>
2020-08-21 11:44:47 +0100

    Header.js

--- src/components/App.js
+++ src/components/App.js
@@ -10,38 +10,26 @@ class App extends React.Component {
 state = {
   isVisible: false,
    articles: [{
-      title: "2020 Cohort",
-      description: ". Nullam euismod sodales venenatis. Morbi ultrices.",
+      title: "Creation timelines for the standard",
+      description: "Nullam euismod sodales venenatis. Morbi ultrices.",
       author: "Josh",
       created_at: Date.now(),
       link: "https://www.bbc.co.uk/news/technology-53849920"
     },
     {
-      title: "let me know when you’re ready",
-      description: ". Nullam euismod sodales venenatis. Morbi ultrices.",
-      author: "Josh",
+      title: "Let me know when you’re ready",
+      description: "Nor is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain.",
+      author: "Kojo",
       created_at: Date.now(),
       link: "https://www.bbc.co.uk/news/education-49249684"
     },
   ]
 }
 
-toggleVisibility = (clickEven) => {
-  console.log('clicked')
-  this.setState((currentState) =>{
-    const newVisibility = !currentState.isVisible;
-    return {...currentState, isVisible: newVisibility }
-  })
-
-}
-  
-
   render() {
     return (
       <div className="App">
         <Header />
-        <button onClick={this.toggleVisibility}>Clicked</button>
-          {this.state.isVisible ? <p>Hello</p> : null} 
         <ArticleList articles={this.state.articles} />
       </div>
     );
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,23 +1,48 @@
 import React from 'react'
 
- const ArticleList = (props) => {
-   console.log(props)
-  return (
-      <ul>
-      {
-        props.articles.map((article) => {
-          return (
-          <li key={article.title}>
-            <h1>{article.title}</h1>
-            <p>{article.description}</p>
-            <h4>{article.author}</h4>
-            <a href={article.link}>article link</a>
-            <p>{article.created_at}</p>
-          </li>
-          )
-        })}
-     </ul>
-  )
+class ArticleList extends React.Component {
+  state = {
+    isOpen: false,
+  };
+  toggleVisibility = (clickEvent) => {
+    this.setState((currentState) => {
+       const newVisibility = !currentState.isOpen;
+       return { ...currentState,isOpen: newVisibility }
+    })
+  }
+
+   render() {
+     return (
+         <ul>
+         {
+           this.props.articles.map((article) => {
+             return (
+             <li className="articles" key={article.title}>
+               <h1 className="article_title"><a href={article.link} target="blank">{article.title}</a></h1>
+               <p className="article_desc">{article.description}</p>
+               <h4>{article.author}</h4>
+               <a href={article.link} target="blank">{article.link}</a>
+               <p>{article.created_at}</p>
+
+               {this.state.isOpen ? <p> Close</p> : null} 
+               <button className="btn" onClick={() => this.toggleVisibility()}>toggle</button>
+             </li>
+             )
+           })}
+        </ul>
+     )
+   }
 }
 
 export default ArticleList;
+
+// toggleVisibility = (clickEven) => {
+//   this.setState((currentState) => {
+//     const newVisibility = !currentState.isVisible;
+//     return {
+//       ...currentState,
+//       isVisible: newVisibility
+//     }
+//   })
+
+// }
