56a77599dbc4fbc646f6c8635a2bcde287216942
Joshua Simpson <simpsonjosh16@gmail.com>
2020-08-21 14:56:28 +0100

    Working on Article toggle button

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -1,22 +1,27 @@
 import React from 'react';
 
-const ArticleCard = ({title, description, link, created_at, author}) => {
-  // const handleClick = (event) => {
+const ArticleCard = ({title, description, link, created_at, author, isOpen}) => {
+  // const handleToggleClick = (event) => {
 
   // }
 
   return (
     <section>
       <li className="articles" key={title}>
-        <h1 className="article_title"><a href={link} target="blank">{title}</a></h1>
-        <p className="article_desc">{description}</p>
-        <h4>{author}</h4>
-        <a href={link} target="blank">{link}</a>
-        <p>{created_at}</p>
+        <h1 className="article_title">{title}</h1>
+        
 
-        {this.state.isOpen ? <p> Close</p> : null}
-        <button className="btn" onClick={() => this.toggleVisibility()}>toggle</button>
-        </li>
+        {isOpen ? 
+        <div>
+          <p className="article_desc">description: {description}</p>
+          <h4>{author}</h4>
+          <a href={link} target="blank">{link}</a>
+          <p>{created_at}</p>
+        </div>
+          : null} 
+
+      <button className="btn" onClick={() => this.toggleVisibility()}>toggle</button>
+      </li>
     </section>
   );
 };
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -5,40 +5,50 @@ class ArticleList extends React.Component {
   
   state = {
     isOpen: false,
-    articles: 
-    {
+    articles: {
+      "Creation timelines for the standard": {
       title: "Creation timelines for the standard",
       description: "Nullam euismod sodales venenatis.Morbi ultrices.",
       author: "Josh",
       created_at: '20 August 2020',
-      link: "https://www.bbc.co.uk/news/technology-53849920"
+      link: "https://www.bbc.co.uk/news/technology-53849920",
+      isOpen: false,
     },
-    {
-      title: "Let me know when you’re ready",
+    
+      "Let me know when you’re ready": {
       description: "Nor is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain.",
       author: "Kojo",
       created_at: '21 August 2020',
-      link: "https://www.bbc.co.uk/news/education-49249684"
+      link: "https://www.bbc.co.uk/news/education-49249684",
+      isOpen: false,
     },
-    
-  };
-  // toggleVisibility = (clickEvent) => {
-    //   this.setState((currentState) => {
+    }
+  }
+  toggleVisibility = (clickEvent) => {
+      // this.setState((currentState) => {
       
-      //      const newVisibility = !currentState.isOpen;
-      //      return { ...currentState,isOpen: newVisibility }
-      //   })
+      //   //    const newVisibility = !currentState.isOpen;
+      //   //    return { ...currentState.articles, [isOpen: newVisibility }
+      //   // })
+      // console.log(clickEvent)
       // }
-      
+  }
       render() {
-       const   articleArray = Object.entries(this.state.articles)
+       const articleArray = Object.entries(this.state.articles)
        console.log(articleArray)
 
        
      return (
-        <ArticleCard />
+       <main>
+         {articleArray.map(([articleName, data]) => {
+          return <ArticleCard key={articleName} {...data}/>
+  
+         })}
+
+       </main>
      )
    }
 }
+
 
 export default ArticleList;
